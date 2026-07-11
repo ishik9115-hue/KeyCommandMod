89f0439b3d7083da7a0fa52e5508dc067b6c1c8a
GitHub Action <action@github.com>
2026-07-11 04:17:29 +0000

    Initialize template repository proper
    Set package name to com.github.ishik9115-hue.keycommandmod
    Set project name to KeyCommandMod
    Set modid to keycommandmod

--- src/main/java/com/github/ishik9115-hue/keycommandmod/init/AutoDiscoveryMixinPlugin.java
+++ src/main/java/com/github/ishik9115-hue/keycommandmod/init/AutoDiscoveryMixinPlugin.java
@@ -1,4 +1,4 @@
-package com.example.init;
+package com.github.ishik9115-hue.keycommandmod.init;
 
 import org.spongepowered.asm.lib.tree.ClassNode;
 import org.spongepowered.asm.mixin.extensibility.IMixinConfigPlugin;
